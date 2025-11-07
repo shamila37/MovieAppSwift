fb798e2a8961c671adbb8c4da01342821e0088d2
ShamilaGunarathna <shamilaashan37@gmil.com>
2025-11-07 16:03:32 +0530

    Initial Commit

---
+++ MovieAppSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieAppSwift
+//
+//  Created by Shamila Ashan Gunarathna on 2025-11-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieAppSwift/MovieAppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppSwiftApp.swift
+//  MovieAppSwift
+//
+//  Created by Shamila Ashan Gunarathna on 2025-11-07.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
